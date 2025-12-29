7e5d925caa3649149fcbd1227fc4f39150f01f06
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-28 21:44:27 -0800

    fix: routing for all-projects

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 )
